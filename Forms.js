255f91df939b307922c65e3a08e28dbf5bd70ef4
Fandresena Ravohary <hei.fandresena@gmail.com>
2023-08-04 21:51:43 +0100

    Feat: add some modification

---
+++ Forms.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const Form = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [items, setItems] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddItem = () => {
+    if (inputValue.trim() !== '') {
+      setItems([...items, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleDeleteItem = (index) => {
+    const updatedItems = items.filter((_, i) => i !== index);
+    setItems(updatedItems);
+  };
+
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleAddItem}>Add</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => handleDeleteItem(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Form;
---
+++ Pages.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from '../components/Form';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Input Form with Add and Delete Buttons</h1>
+      <Form />
+    </div>
+  );
+};
+
+export default Home;
